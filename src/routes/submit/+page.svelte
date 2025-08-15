dc4d112bfc5b41d2b845cafc89b856f11112dbb9
Gus Hogg-Blake <gus@gushogg-blake.com>
2025-08-15 16:20:00 +0100

    wip

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 import {onMount, getContext} from "svelte";
+import {n} from "utils/formatNumbers";
 
 let {
 	data,
 	form,
 } = $props();
 
-let uid = $state(form.uid);
+let uid = $state(form?.uid);
+let submissionsReceived = $state(data.submissionsReceived);
+
+console.log(submissionsReceived);
 </script>
 
 <style lang="scss">
@@ -14,12 +18,14 @@ let uid = $state(form.uid);
 </style>
 
 <div id="main">
-	<p> Thanks for taking part in the survey!
-	<p> Your submission ID is <code>{uid}</code>.
-	<p> <b>If you wish to update or delete your responses, save this ID now as it will not be available after closing the page!</b>
-	<p> The results will be published in September 2025.
-	<p> Subscribe to <a href="//misocfd.substack.com">Misophonia and Craniofacial Dystrophy</a> to receive updates.
-	<br><br>
-	<p align=center> <a href="/">Back to survey</a>
-	<p> &nbsp;
+	<p> Thanks for taking part in the survey!</p>
+	{#if uid}
+		<p> Your submission ID is <code>{uid}</code>.</p>
+		<p> <b>If you wish to update or delete your responses, save this ID now as it will not be available after closing the page!</b></p>
+	{/if}
+	<p> The results will be published in September 2025.</p>
+	<p> Subscribe to <a href="//misocfd.substack.com">Misophonia and Craniofacial Dystrophy</a> to receive updates.</p>
+	<p> Submissions received: {n(submissionsReceived)}.</p>
+	<p align=center> <a href="/">Back to survey</a></p>
+	<p> &nbsp;</p>
 </div>
